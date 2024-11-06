b622b852dd25226184cf7342e042ffe9cc320c67
sultanfurkhan <sultanfurkhan27@gmail.com>
2024-11-06 20:36:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 def lambda_handler(event, context):
-  client = boto3.client('ec2')
+  client = boto3.client('rds')
   responce = client.run_instances(
     ImageId='ami-0f71013b2c8bd2c29',
     InstanceType='t2.micro',
