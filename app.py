29b3a604c1b45641cfbe43581e1f85ab8aef039c
furkhan <sultanfurkhan27@gmail.com>
2024-11-06 19:43:19 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+def lambda_handler(event, context):
+  client = boto3.client('ec2')
+  responce = client.run_instances(
+    ImageId='ami-0f71013b2c8bd2c29',
+    InstanceType='t2.micro',
+    KeyName='awskey',
+    MaxCount=1,
+    MinCount=1
+)
