c86e2837a6bc68fbb770b472fe5287faa9206f3e
sultanfurkhan <sultanfurkhan27@gmail.com>
2024-11-06 19:52:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ def lambda_handler(event, context):
   client = boto3.client('ec2')
   responce = client.run_instances(
     ImageId='ami-0f71013b2c8bd2c29',
-    InstanceType='t2.small',
-    KeyName='mykey',
+    InstanceType='t2.xl',
+    KeyName='yourkey',
     MaxCount=1,
     MinCount=1
 )
