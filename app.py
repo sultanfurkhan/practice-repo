79504173bece83220934bec44edeb6b21d886fb2
furkhan <sultanfurkhan27@gmail.com>
2024-11-06 20:37:52 +0530

    s3

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import json
 import boto3
 def lambda_handler(event, context):
-  client = boto3.client('ec2')
-  responce = client.run_instances(
+  client = boto3.client('s3')
+  responce = client.run_isnstances(
     ImageId='ami-0f71013b2c8bd2c29',
-    InstanceType='t2.micro',
+    InstanceType='t2.micro3',
     KeyName='awskey',
     MaxCount=1,
     MinCount=1
